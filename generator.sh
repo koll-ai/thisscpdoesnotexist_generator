0c4e6df0f4802da4a2d2aa8fb55bc7a11d8078ff
Phil <filouface@gmail.com>
2021-11-11 15:24:01 +0100

    update paths

--- generator.sh
+++ generator.sh
@@ -2,14 +2,14 @@
 
 python3 generate_scp.py
 
-cd $HOME/SCP-GPT_db/
+cd $HOME/SCP_BDD/
 
 git add *
 git commit -m "added new SCP-GPT"
 git pull
 git push
 
-cd $HOME/tsde/
+cd $HOME/SCP_API/
 git add last.txt
 git commit -m "update last SCP"
 git pull
