fad8312656c6edfdafa7516e8ec6b1075329b128
Phil <filouface@gmail.com>
2022-01-23 16:29:22 +0100

    pep8

--- scp_gen.py
+++ scp_gen.py
@@ -9,43 +9,47 @@ object_classes = ['Safe', 'Euclid', 'Keter', 'Thaumiel']
 stops = ['\nItem #:', '\nDescription:', '\nAddendum', '\nRecovery:']
 username = "-1"
 
+
 def connect():
     key = open("openai.key", "r").read().rstrip()
     openai.api_key = key
     print("connected to openAI")
 
-def req_complete(prompt, max_tokens):
+
+def req_complete(prompt, max_tokens, temerature=0.7):
     text = openai.Completion.create(
-    engine="davinci",
-    prompt=prompt,
-    temperature=0.7,
-    max_tokens=max_tokens,
-    top_p=1.0,
-    frequency_penalty=1.0,
-    presence_penalty=0.0,
-    stop=stops,
-    user=username
+        engine="davinci",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=max_tokens,
+        top_p=1.0,
+        frequency_penalty=1.0,
+        presence_penalty=0.0,
+        stop=stops,
+        user=username
     )["choices"][0]["text"]
 
     return text
 
-def remove_last_sentence(s):
-    return s[::-1].split(".", 1)[1][::-1]+'.'
 
-def generate_scp(scp_number, description, object_class):
-    #get current hour from time module
+def remove_last_sentence(s):
+    return s[::-1].split(".", 1)[1][::-1] + '.'
+
+
+def generate_scp(scp_number, description, object_class, **kwargs):
+    # get current hour from time module
     global username
-    username = str(int(time.time()/3600))
+    username = str(int(time.time() / 3600))
 
     prompt = 'SCP-' + str(scp_number) + ' is ' + description + '.\n\n' \
-        + 'Item #: ' + 'SCP-' + scp_number + '\n\n' \
-        + 'Object Class: ' + object_class + '\n\n' \
-        + 'Special Containment Procedures: '
+             + 'Item #: ' + 'SCP-' + scp_number + '\n\n' \
+             + 'Object Class: ' + object_class + '\n\n' \
+             + 'Special Containment Procedures: '
     ret = req_complete(prompt, 300)
 
     if getSafetyLabel(ret) == 2:
         return ERROR_UNSAFE_CONTENT
- 
+
     prompt += ret + "\n\nDescription:"
     ret = req_complete(prompt, 500)
 
@@ -58,13 +62,13 @@ def generate_scp(scp_number, description, object_class):
     if getSafetyLabel(ret) == 2:
         return ERROR_UNSAFE_CONTENT
 
-    prompt += ret + "\n\nAddendum " + str(scp_number) +".1:"
+    prompt += ret + "\n\nAddendum " + str(scp_number) + ".1:"
     ret = req_complete(prompt, 600)
 
     if getSafetyLabel(ret) == 2:
         return ERROR_UNSAFE_CONTENT
 
-    prompt += ret + "\n\nAddendum " + str(scp_number) +".2:"
+    prompt += ret + "\n\nAddendum " + str(scp_number) + ".2:"
     ret = req_complete(prompt, 300)
 
     if getSafetyLabel(ret) == 2:
@@ -74,12 +78,13 @@ def generate_scp(scp_number, description, object_class):
 
     return prompt
 
+
 def getSafetyLabel(text):
     content_to_classify = text
 
     response = openai.Completion.create(
         engine="content-filter-alpha",
-        prompt = "<|endoftext|>"+content_to_classify+"\n--\nLabel:",
+        prompt="<|endoftext|>" + content_to_classify + "\n--\nLabel:",
         temperature=0,
         max_tokens=1,
         top_p=1,
@@ -87,7 +92,7 @@ def getSafetyLabel(text):
         presence_penalty=0,
         logprobs=10,
         user=username
-        )
+    )
 
     output_label = response["choices"][0]["text"]
 
@@ -129,32 +134,33 @@ def getSafetyLabel(text):
 
     return output_label
 
+
 def toHTML(text):
     print(text)
 
-    split = text.split('\n',1)
-    text = "<center> <h3> <i>" + split[0] +"</i> </h3> </center>"+ split[1]
+    split = text.split('\n', 1)
+    text = "<center> <h3> <i>" + split[0] + "</i> </h3> </center>" + split[1]
 
-    #entre guillemmets en italique
+    # entre guillemmets en italique
     text = re.sub(r'"([^"]*)"', r'<i>"\1"</i>', text)
 
-    #termes insérés <=> toujous présents
+    # termes insérés <=> toujous présents
     for s in ["Item #:", "Object Class:", "Special Containment Procedures:", "Description:", "Recovery:"]:
-        text = re.sub(r"" + s,r"<h3>" + s +"</h3>",text)
-        
-    text = re.sub(r'Addendum ?(\d*)\.([^\d]*)(\d):',r"<h3>Addendum \1.\3 :</h3>",text)
+        text = re.sub(r"" + s, r"<h3>" + s + "</h3>", text)
+
+    text = re.sub(r'Addendum ?(\d*)\.([^\d]*)(\d):', r"<h3>Addendum \1.\3 :</h3>", text)
 
     text = re.sub("\n", "<br>", text)
 
     text = re.sub(r"(<br>){3,}", "<br><br>", text)
 
-    #mot avant ":" en gras
+    # mot avant ":" en gras
     text = re.sub(r'([0-9A-Za-z: #\-█]{4,}:)', r"<br>\1", text)
 
-    #nom du scp en italique
+    # nom du scp en italique
     text = re.sub(r"SCP\-([0-9]*)", r"<i>SCP-\1</i>", text)
 
     text = "<div class='justifier'>" + text + "</div>"
     text = "<style>.justifier {  text-align: justify;  text-justify: inter-word;}</style>" + text
-    
+
     return text
